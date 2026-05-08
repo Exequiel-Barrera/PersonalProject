6fcd5e28e9e8f8d3b8674cc7bf9b36bce743a053
Exequiel-Barrera <exequielbm86@gmail.com>
2026-05-08 12:04:04 +1200

    hardcoded machine names on app.tsx

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,15 +1,22 @@
-import { useFruits } from '../hooks/useFruits.ts'
-
 function App() {
-  const { data } = useFruits()
+  const machines = [
+    'Excavator (a)',
+    'Loader (b)',
+    'Dump Truck (c)',
+  ]
 
   return (
-    <>
-      <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
-      </div>
-    </>
+    <div className="app">
+      <h1>Heavy Machinery Pre-Start Inspection App</h1>
+
+      <h2>Select Machine</h2>
+
+      <ul>
+        {machines.map((machine) => (
+          <li key={machine}>{machine}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
--- client/routes.tsx
+++ client/routes.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable react/jsx-key */
 import { createRoutesFromElements, Route } from 'react-router'
+
 import App from './components/App'
 
-const routes = createRoutesFromElements(<Route index element={<App />} />)
+const routes = createRoutesFromElements(
+  <>
+    <Route path="/" element={<App />} />
+  </>
+)
 
 export default routes
